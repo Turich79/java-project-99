270c372b42cfb4b81c42b3a127c7db89089f22a4
Artur Neustroev <turich79@mail.ru>
2025-01-07 10:40:02 +0700

    step 5

--- app/src/test/java/hexlet/code/app/controller/api/UsersControllerTest.java
+++ app/src/test/java/hexlet/code/app/controller/api/UsersControllerTest.java
@@ -37,8 +37,10 @@ import net.datafaker.Faker;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+//import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+//@ActiveProfiles(value = "development")
 @AutoConfigureMockMvc
 public class UsersControllerTest {
 
